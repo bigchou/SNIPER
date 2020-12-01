744462a2ec77e3fb7b20bab3ac7a57f087a3a9cc
bigchou <40347905s@gmail.com>
2020-12-01 20:49:47 +0800

    clean run.py and README

--- run.py
+++ run.py
@@ -28,20 +28,24 @@ import PIL.Image
 
 def parse_args():
     parser = argparse.ArgumentParser(description='RFCN Demo')
-    parser.add_argument('--inputdir', default='./demo/input_imgs/',help="path to input directory")#'./demo/input_imgs/'#'./demo/mytake/'#./demo/products-yahoo/
-    parser.add_argument('--outputdir',default='./myoutput/',help="path to output directory")#'./myoutput/'#'./mytaketest/'#./products-yahoo/
-    parser.add_argument('--batchsize',default=1000,type=int,help="size of batch processing")
-    parser.add_argument('--conf_thres',default=0.3,type=float,help="keep the candidates whose confidence value is higher than confidence threshold. If the number of filtered candidate is zero, we keep the one with the largest confidence as the detection result")
-    parser.add_argument('--topk',default=1,type=int,help="obtain top-k detection results if number of possible predictions is larger than topk value")
+    parser.add_argument('--inputdir', default='demo/ooo',help="path to input directory")#'./demo/input_imgs/'#'./demo/mytake/'#./demo/products-yahoo/
+    parser.add_argument('--outputdir',default='oootest',help="path to output directory")#'./myoutput/'#'./mytaketest/'#./products-yahoo/
+    parser.add_argument('--cropjson',default='bbox_info.json',help="path to output json file including bounding box coordinate information")
+    parser.add_argument('--batchsize',default=1000,type=int,help="size of batch processing to speed up detection")
+    parser.add_argument('--conf_thres',default=0.3,type=float,help="keep the candidates whose confidence value is higher than confidence threshold. If the number of filtered candidate is zero, we keep the at least one with the largest confidence as the detection result")
+    parser.add_argument('--topk',default=1,type=int,help="obtain at most k detection results. if the number of possible results is lower than k, the number of outputs will be lower than k")
     parser.add_argument('--mode',default='crop',choices=['draw', 'crop'],type=str,help="image cropping or drawing")
-    parser.add_argument('--nonms', action='store_true',help="set true to keep redundant bounding boxes")
+    parser.add_argument('--nonms', action='store_true',help="set true to keep redundant bounding boxes (i.e. disable non-maximum suppression)")
     args = parser.parse_args()
     return args
-
+args = parse_args()
 """
 # Cropping Example
-python run.py --mode crop --topk 1 --nonms  # time-efficient
+python run.py --mode crop --topk 100
+python run.py --mode crop --topk 1 --nonms
 python run.py --mode crop --topk 1
+
+
 
 # Drawing Example
 python run.py --mode draw --topk 2
@@ -49,14 +53,22 @@ python run.py --mode draw --topk 5
 python run.py --mode draw --topk 5 --nonms
 """
 
+# colorcode in (B,G,R) format
+colorcode = {}
+interval = int(255 / args.topk)
+for i, v in enumerate(range(0,256,interval)):
+    blue, green, red = 255-v, v, v
+    colorcode[i] = (blue, green, red)
+
+
 args = parse_args()
-if args.mode == "crop":
-    if args.topk > 1:
-        assert False, "Image Cropping Mode should set topk to be 1 only"
+
+if not os.path.exists(args.outputdir):
+    os.mkdir(args.outputdir)
 
 def nms_python(bboxes,pscores,threshold):
     '''
-    #https://github.com/satheeshkatipomu/nms-python/blob/master/NMS%20using%20Python%2C%20Tensorflow.ipynb
+    # Modified from https://github.com/satheeshkatipomu/nms-python/blob/master/NMS%20using%20Python%2C%20Tensorflow.ipynb
     NMS: first sort the bboxes by scores , 
         keep the bbox with highest score as reference,
         iterate through all other bboxes, 
@@ -64,7 +76,7 @@ def nms_python(bboxes,pscores,threshold):
         if iou is greater than threshold,then discard the bbox and continue.
         
     Input:
-        bboxes(numpy array of tuples) : Bounding Box Proposals in the format (x_min,y_min,x_max,y_max).
+        bboxes(numpy array of tuples) : Bounding Box Proposals in the format (x_min,y_min,x_max,y_max,confidence).
         pscores(numpy array of floats) : confidance scores for each bbox in bboxes.
         threshold(float): Overlapping threshold above which proposals will be discarded.
         
@@ -113,19 +125,18 @@ def nms_python(bboxes,pscores,threshold):
         sorted_idx = np.delete(sorted_idx,delete_idx)
         
     
-    #Return filtered bboxes and theirs corresponding confidence value
-    return bboxes[filtered].astype('int'), pscores[filtered]
-
-
-def process_mul_scores(scores, xcls_scores):
-    """
-    Do multiplication of objectness score and classification score to obtain the final detection score.
-    """
-    final_scores = np.zeros((scores.shape[0], xcls_scores.shape[1]+1))
-    final_scores[:, 1:] = xcls_scores[:, :] * scores[:, [1]]
-    return final_scores
+    #Return filtered bboxes
+    return bboxes[filtered]
 
 def batch_proc(sym,image_names_full):
+    '''
+    Input:
+        sym(symbol objects of mxnet) : detection network
+        image_names_full(list of strings) : list of string store images full path. e.g. [products-momo/xxx.jpg, products-momo/ooo.jpg]
+
+    Output:
+        output : [inputfile, (y1,x1,y2,x2,conf)] where inputfile stores input image filepath and (y1,x1,y2,x2,conf) stores mulitple object proposal coordinates and their corresponding confidence value.
+    '''
     # load raw data(image)
     expand_ratio_list = []
     data = []
@@ -193,12 +204,11 @@ def batch_proc(sym,image_names_full):
     # extract roi_pooled features for evaluation / visualization
     rois = []
     objectness_scores = []
-    roipooled_features = []
+    
 
     # get eval data based on the train val split
     count = 0
     total = len(eval_data)
-    #bp()
     for idx in range(len(eval_data)):
         count += 1
         data_batch = mx.io.DataBatch(data=[eval_data[idx]], label=[], pad=0, index=idx,
@@ -210,8 +220,6 @@ def batch_proc(sym,image_names_full):
                                                             pooled_size=(7, 7), spatial_scale=0.0625)
         pooled_feat = mx.ndarray.Pooling(data=roipooled_conv5_feat, pool_type='avg', global_pool=True, kernel=(7, 7))
         #pooled_feat.shape = (300L, 3072L, 1L, 1L)
-        roipooled_features.append(pooled_feat.reshape((pooled_feat.shape[0], -1)).asnumpy())
-        #roipooled_features[0].shape = (300, 3072)
         roi_this = bbox_pred(mod.get_outputs()[0].asnumpy().reshape((-1, 5))[:, 1:], np.array([0.1, 0.1, 0.2, 0.2]) * mod.get_outputs()[2].asnumpy()[0])
         #roi_this.shape = (300, 4)
         roi_this = clip_boxes(roi_this, im_info_list_eval[idx][0][:2])
@@ -225,20 +233,6 @@ def batch_proc(sym,image_names_full):
     #objectness_scores.shape = (39, 1, 300, 2)
     output = []
     for idx in range(len(rois)):
-        # ************************************************************
-        # load image with original size
-        im_name = image_names_eval[idx]
-        #print('testing {}'.format(im_name))
-        ori_im = PIL.Image.open(im_name)
-        if ori_im.mode == "RGB":
-            ori_im = cv2.imread(im_name, cv2.IMREAD_COLOR)
-        else:
-            ori_im = ori_im.convert("RGBA")
-            new_canvas = PIL.Image.new("RGB",ori_im.size,"WHITE")
-            new_canvas.paste(ori_im, (0,0), ori_im)
-            ori_im = cv2.cvtColor(np.array(new_canvas), cv2.COLOR_RGB2BGR)
-        im = ori_im
-        # ************************************************************
         scale = im_info_list_eval[idx][0][2]
         pscores = objectness_scores[idx][0][:, [1]].flatten()#obtain confidence value of all proposals
         if args.nonms:
@@ -253,12 +247,12 @@ def batch_proc(sym,image_names_full):
                     x2 *= expand_ratio_list[idx][1]
                     y1 *= expand_ratio_list[idx][0]
                     y2 *= expand_ratio_list[idx][0]
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    x1, y1, x2, y2, conf = int(x1), int(y1), int(x2), int(y2), float(pscores[max_index])
                     #print("bounding box info:",x1,y1,x2,y2)
-                    filtered_boxes_list.append((x1,y1,x2,y2))
+                    filtered_boxes_list.append((x1,y1,x2,y2,conf))
             boxes_list = filtered_boxes_list
         else:
-            # REMOVE REDUNDANT BOUNDING BOXES WITH NMS
+            # Remove Redundant Bounding Boxes with Non-Maximum Suppression (NMS)
             bboxes = []
             for p in range(len(pscores)):
                 boxes = rois[idx][p].astype('f')
@@ -267,47 +261,43 @@ def batch_proc(sym,image_names_full):
                 x2 *= expand_ratio_list[idx][1]
                 y1 *= expand_ratio_list[idx][0]
                 y2 *= expand_ratio_list[idx][0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                bboxes.append((x1,y1,x2,y2))
-            bboxes = np.array(bboxes)
-            bboxes_after_nms, pscores_after_nms = nms_python(bboxes,pscores,0.0)
+                x1, y1, x2, y2, conf = int(x1), int(y1), int(x2), int(y2), float(pscores[p])#casting
+                bboxes.append((x1,y1,x2,y2,conf))
+            bboxes = np.array(bboxes)#dtype=float64
+            bboxes_after_nms = nms_python(bboxes,pscores,0.0)#dtype=float64
             filtered_boxes_list = []
-            for it, pscore in enumerate(pscores_after_nms):
-                if it == 0 or pscore > args.conf_thres:#keep at least one candidate and remove the remaining candidates whose confidence value is lower than threshold
-                    filtered_boxes_list.append(bboxes_after_nms[it])
+            for it, bbbb in enumerate(bboxes_after_nms):
+                x1, y1, x2, y2, pscore = bbbb
+                x1, y1, x2, y2, conf = int(x1), int(y1), int(x2), int(y2), float(pscore)
+                if it == 0 or conf > args.conf_thres:#keep at least one candidate and remove the remaining candidates whose confidence value is lower than threshold
+                    filtered_boxes_list.append((x1,y1,x2,y2,conf))#casting
             boxes_list = filtered_boxes_list
-        # Crop or Draw
-        maxlen = min(len(boxes_list),args.topk)
-        for boxes in boxes_list[:maxlen]:
-            x1, y1, x2, y2 = boxes
-            if args.mode == "draw":
-                # draw boudning box
-                cv2.rectangle(im, (x1, y1), (x2, y2), (255,0,0), 5)#thickness=5
-            else:
-                # image cropping
-                im = im[y1:y2,x1:x2] # Run one iteration only because image cropping mode should set topk to be 1
-        # Save
-        if not os.path.exists(args.outputdir):
-            os.mkdir(args.outputdir)
-        outfile = args.outputdir+os.path.basename(image_names_full[idx])
-        #print("save to {}".format(outfile))
-        #cv2.imwrite(outfile,im)
-        output.append([outfile, im, (y1,x1,y2,x2)])
+        # Filter again
+        num_box = min(len(boxes_list),args.topk)
+        chosen_boxes_list = boxes_list[:num_box]
+        # prepare output format
+        tmp = []
+        for it, boxes in enumerate(chosen_boxes_list):
+            x1, y1, x2, y2, conf = boxes
+            tmp.append((y1,x1,y2,x2,conf))
+        inputfile = image_names_full[idx]
+        output.append([inputfile,tmp])
     return output
 
 def main():
-    store = args.inputdir.split("/")[-2]#products-yahoo
-    # get symbol
+    # get symbol (a symbol can be regarded as a network in mxnet)
     pprint.pprint(config)
     sym_inst = resnet_mx_101_e2e_3k_demo()
     sym = sym_inst.get_symbol_rcnn(config, is_train=False)
-    # load data
-    image_names_full = glob(args.inputdir+"*")#e.g. './demo/input_imgs/*'
-    # batch processing
+    # load all images
+    image_names_full = glob(os.path.join(args.inputdir,"*"))#./demo/input_imgs/*
+    # prepare a dictionary to store bounding box information
     cropcoord_dict = {}
-    if os.path.exists(store+".json"):
-        with open(store+".json","r") as f:
+    # continue from last run
+    if os.path.exists(args.cropjson):
+        with open(args.cropjson,"r") as f:
             cropcoord_dict = json.load(f)
+    # main loop
     for i in tqdm(range(0,len(image_names_full),args.batchsize)):
         # continue or not
         continue_flag = False
@@ -317,15 +307,41 @@ def main():
                 continue_flag = True
                 break
         if continue_flag: continue
-        # process
+        # batch processing
         batch_image_names_full = image_names_full[i:i+args.batchsize]
         output = batch_proc(sym,batch_image_names_full)
-        for (outfile, im, (y1,x1,y2,x2)) in output:
-            cv2.imwrite(outfile,im)
-            fname = os.path.join(store,os.path.basename(outfile))
-            cropcoord_dict[fname] = [y1,x1,y2,x2]
-        with open(store+".json","w") as f:
-            json.dump(cropcoord_dict,f)
+        # output
+        for out in output:
+            # image part
+            im_name, bboxlist = out
+            im = PIL.Image.open(im_name)
+            if im.mode == "RGB":
+                im = cv2.imread(im_name, cv2.IMREAD_COLOR)
+            else:
+                im = im.convert("RGBA")
+                new_canvas = PIL.Image.new("RGB",im.size,"WHITE")
+                new_canvas.paste(im, (0,0), im)
+                im = cv2.cvtColor(np.array(new_canvas), cv2.COLOR_RGB2BGR)
+            for it, boxes in enumerate(bboxlist):
+                y1, x1, y2, x2, conf = boxes
+                if args.mode == "crop":
+                    # image cropping
+                    outfile = os.path.join(args.outputdir,str(it)+"_"+os.path.basename(im_name))
+                    crop_im = im[y1:y2,x1:x2]
+                    cv2.imwrite(outfile,crop_im)
+                else:
+                    # draw boudning box
+                    cv2.rectangle(im, (x1, y1), (x2, y2), colorcode[it], 5)#thickness=5
+                    cv2.putText(im, "%2.1f%%"%(conf*100.0), (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, colorcode[it], 2)
+                    if it == len(bboxlist)-1:
+                        outfile = os.path.join(args.outputdir,os.path.basename(im_name))
+                        cv2.imwrite(outfile,im)
+            # json part
+            fname = os.path.join(args.inputdir,os.path.basename(im_name))
+            cropcoord_dict[fname] = bboxlist
+        # output bounding box information
+        with open(args.cropjson,"w") as f:json.dump(cropcoord_dict,f)
+        print("output bounding box information to %s"%(args.cropjson))
     
 
 if __name__ == '__main__':
